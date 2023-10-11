44dfb4a5027b77419e63ab73e57969cc06154471
Mahalakshmi <116434881+Mahalaskhmi02@users.noreply.github.com>
2023-10-10 21:32:00 -0700

    password.java

--- password.java
+++ password.java
@@ -14,11 +14,11 @@ public class PasswordShowHideApp {
         JFrame frame = new JFrame("Password Show/Hide Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create a password field to input the password
+
         JPasswordField passwordField = new JPasswordField(20);
         passwordField.setEchoChar('*'); // Set the echo character to hide the password
 
-        // Create a button to show/hide the password
+        
         JButton showHideButton = new JButton("Show Password");
         showHideButton.addActionListener(new ActionListener() {
             boolean isPasswordVisible = false;
@@ -36,12 +36,12 @@ public class PasswordShowHideApp {
             }
         });
 
-        // Create a panel to hold the components
+        
         JPanel panel = new JPanel();
         panel.add(passwordField);
         panel.add(showHideButton);
 
-        // Add the panel to the frame
+        
         frame.add(panel);
 
         frame.pack();
